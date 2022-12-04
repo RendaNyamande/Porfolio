96854714be6437bb00377c4799c2ac3bbac1c17f
Renda Nyamande <rendanyamande44@gmail.com>
2022-12-04 23:01:54 +0200

    Email

--- pages/api/contacting.js
+++ pages/api/contacting.js
@@ -1,7 +1,8 @@
+
+let nodemailer = require("nodemailer");
 export default async function (req, res) {
   require("dotenv").config();
   const PASSWORD = process.env.password;
-  let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
@@ -33,4 +34,4 @@ export default async function (req, res) {
   });
   console.log(req.body);
   res.send("success");
-}
+};
